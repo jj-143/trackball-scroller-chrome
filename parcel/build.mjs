dc51d12c960ee32a542878ad80c68ee01097c1cd
JJ Kim <jj.dev.001@gmail.com>
2023-05-14 20:43:57 +0900

    build: don't use HMR on production

--- parcel/build.mjs
+++ parcel/build.mjs
@@ -30,6 +30,7 @@ function clean() {
 
 function build({ serve }) {
   const distDir = getDistDir()
+  const useHMR = process.env.NODE_ENV !== "production"
 
   const bundler = new Parcel({
     entries: [
@@ -45,15 +46,17 @@ function build({ serve }) {
     // Serving entry files to develop locally without needing to load it as an Extension.
     // NOTE: Using these options causes Chrome to display a CSP issue but it's harmless;
     // Parcel's hmr-runtime is merely checking on the availability of eval().
-    serveOptions: {
-      port: 3000,
-    },
-    hmrOptions: {
-      // Using different port. see: https://github.com/parcel-bundler/parcel/issues/6061
-      // this might be resolved (with a new build setup) if we use a plugin
-      // @parcel/config-webextension when migrating to Manifest V3.
-      port: 3001,
-    },
+    ...(useHMR && {
+      serveOptions: {
+        port: 3000,
+      },
+      hmrOptions: {
+        // Using different port. see: https://github.com/parcel-bundler/parcel/issues/6061
+        // this might be resolved (with a new build setup) if we use a plugin
+        // @parcel/config-webextension when migrating to Manifest V3.
+        port: 3001,
+      },
+    }),
   })
 
   console.info("[*] distDir:", distDir)
