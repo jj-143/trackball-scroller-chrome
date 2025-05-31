5bacd479682569ebebc2caa7bad99a35f1ff6f83
JJ Kim <jj.dev.001@gmail.com>
2025-05-31 15:09:23 +0900

    etc: Set screenshot size to 1440x900

--- cypress/utils/screenshot.cy.ts
+++ cypress/utils/screenshot.cy.ts
@@ -1,5 +1,5 @@
-it("Screenshots", () => {
-  cy.viewport("macbook-13")
+it("Options page screenshot (1440x900)", () => {
+  cy.viewport(1440, 900)
   cy.visit("/options/options.html")
-  cy.screenshot({ capture: "fullPage" })
+  cy.screenshot({ capture: "fullPage", overwrite: true })
 })
